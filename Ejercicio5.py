71901e9f010328fd0ae182e3484cea30d6e7b845
Sebasval1122 <svalencia170@soyudemedellin.edu.co>
2024-02-18 16:21:42 -0500

    Ejercicios 4 y 5

---
+++ Ejercicio5.py
@@ -0,0 +1,4 @@
+""""Crear una función que convierta grados Fahrenheit a grados Celsius."""
+fah=int(input("Ingrese los grados Fahrenheit: "))
+cel=(fah-32)*5/9
+print("Los grados Celsius son: ",cel)
---
+++ Ejercico4.py
@@ -0,0 +1,6 @@
+"""". Escribir un programa que determine si un número dado es par o impar."""
+numero=int(input("Ingrese un numero: "))
+if numero%2==0:
+    print("El numero es par")
+else:
+    print("El numero es impar")
